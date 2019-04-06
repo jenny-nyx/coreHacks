fbb37f149110076f8827c424e537f876125a43fc
Andy Nguyen <anguye13@binghamton.edu>
2019-04-06 18:28:16 -0400

    pp

--- Controller.py
+++ Controller.py
@@ -1,8 +1,8 @@
 import time, pygame, os, buttons, char, sys
 
 class Controller:
-  def __init__(self, height = 1920, width=1080):
-    self.state = "game";
+  def __init__(self, height = 1280, width=800):
+    self.state = "start";
     self.height = height;
     self.width = width;
     self.screen = pygame.display.set_mode((self.height, self.width));
@@ -15,6 +15,8 @@ class Controller:
 
   def mainLoop(self):
     while True:
+      if (self.state == "start"):
+        self.startingMenu()
       if (self.state == "game"):
         self.gameLoop();
       if (self.state == "gameover"):
@@ -24,15 +26,11 @@ class Controller:
 
   def gameLoop(self):
     while self.state == "game":
-      self.startingMenu()
       for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
           mouseLocation = pygame.mouse.get_pos()
-          if self.startButton.rect.collidepoint(mouseLocation):
-            self.spawnRoom()
-          if self.quitButton.rect.collidepoint(mouseLocation):
-            pygame.quit
-            sys.exit()
+        if event.type == pygame.QUIT:
+          sys.exit()
       pygame.display.flip()
       self.all_sprites.draw(self.screen)
 
@@ -52,13 +50,25 @@ class Controller:
           sys.exit();
 
   def startingMenu(self):
-    image = pygame.transform.scale(pygame.image.load(os.path.join(os.getcwd(), "startUpMenu.png")), (self.height, self.width))
-    self.screen.blit(image, (0, 0))
-    for s in self.buttons:
-      self.screen.blit(s.image, s.rect.topleft)
-    pygame.display.flip()
+    while True:
+      mouseLocation = pygame.mouse.get_pos()
+      image = pygame.transform.scale(pygame.image.load(os.path.join(os.getcwd(), "startUpMenu.png")), (self.height, self.width))
+      self.screen.blit(image, (0, 0))
+      for s in self.buttons:
+        self.screen.blit(s.image, s.rect.topleft)
+      pygame.display.flip()
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          sys.exit()
+        if self.startButton.rect.collidepoint(mouseLocation):
+          self.spawnRoom()
+          return None
+        if self.quitButton.rect.collidepoint(mouseLocation):
+          pygame.quit
+          sys.exit()          
 
   def spawnRoom(self):
+    self.state = "game"
     image = pygame.transform.scale(pygame.image.load(os.path.join(os.getcwd(), "spawn.png")), (self.height, self.width))
     self.screen.blit(image, (0,0))
     coreLounge = pygame.transform.scale(pygame.image.load(os.path.join(os.getcwd(), "coreLounge.png")), (400, 300))
