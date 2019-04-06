0b5bb667c77d9a9f5c6a536ec9ba33c68e7daa80
Andy Nguyen <anguye13@binghamton.edu>
2019-04-06 16:26:34 -0400

    pp

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-def main():
+import sys
+import pygame
 
-main()
+def run_game():  # Initializes game and creates screen object
+  pygame.init()
+  wn = pygame.display.set_mode((1200,800))
+  pygame.display.set_caption("Core Boiz")
+
+  # Main loop for game
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+
+    pygame.display.flip()
+
+
+run_game()
