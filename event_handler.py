00371aaa99678ffbca5b96649b9ad17c8d3cfffb
Jennifer Thakkar <jthakka1@binghamton.edu>
2019-04-10 23:08:16 -0400

    ¯\_(ツ)_/¯

--- event_handler.py
+++ event_handler.py
@@ -23,8 +23,10 @@ class Mixin:
                     player.moving_up = True
                 elif event.key == pygame.K_DOWN:
                     player.moving_down = True
-                elif event.key == pygame.K_q:
+                elif event.key == pygame.K_BACKQUOTE: # ` or tilda
                     self.answer = ""
+                elif event.key == pygame.BACKSPACE:
+                    self.answer -= self.answer[answer.length-1]
                 elif event.key == pygame.K_1:
                     self.answer += "1"
                 elif event.key == pygame.K_2:
@@ -45,6 +47,58 @@ class Mixin:
                     self.answer += "9"
                 elif event.key == pygame.K_0:
                     self.answer += "0"
+                elif event.key == pygame.K_a:
+                    self.answer += "a"
+                elif event.key == pygame.K_b:
+                    self.answer += "b"
+                elif event.key == pygame.K_c:
+                    self.answer += "c"
+                elif event.key == pygame.K_d:
+                    self.answer += "d"
+                elif event.key == pygame.K_e:
+                    self.answer += "e"
+                elif event.key == pygame.K_f:
+                    self.answer += "f"
+                elif event.key == pygame.K_g:
+                    self.answer += "g"
+                elif event.key == pygame.K_h:
+                    self.answer += "h"
+                elif event.key == pygame.K_i:
+                    self.answer += "i"
+                elif event.key == pygame.K_j:
+                    self.answer += "j"
+                elif event.key == pygame.K_k:
+                    self.answer += "k"
+                elif event.key == pygame.K_l:
+                    self.answer += "l"
+                elif event.key == pygame.K_m:
+                    self.answer += "m"
+                elif event.key == pygame.K_n:
+                    self.answer += "n"
+                elif event.key == pygame.K_o:
+                    self.answer += "o"
+                elif event.key == pygame.K_p:
+                    self.answer += "p"
+                elif event.key == pygame.K_q:
+                    self.answer += "q"
+                elif event.key == pygame.K_r:
+                    self.answer += "r"
+                elif event.key == pygame.K_s:
+                    self.answer += "s"
+                elif event.key == pygame.K_t:
+                    self.answer += "t"
+                elif event.key == pygame.K_u:
+                    self.answer += "u"
+                elif event.key == pygame.K_v:
+                    self.answer += "v"
+                elif event.key == pygame.K_w:
+                    self.answer += "w"
+                elif event.key == pygame.K_x:
+                    self.answer += "x"
+                elif event.key == pygame.K_y:
+                    self.answer += "y"
+                elif event.key == pygame.K_z:
+                    self.answer += "z"
                 elif event.key == pygame.K_RETURN:
                     # Checks answer for jordan's room
                     if self.state == 'jordan':
